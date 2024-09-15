b0d5d8c33d49de08d15d011ec622b212cbfda078
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-09-15 14:34:30 +0530

    feat : item collection

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,44 +5,44 @@ function Navbar() {
   return (
     <div>
       <div className="flex h-[4vh] w-full items-center  justify-center bg-[#1D1D1D] ">
-        <h1 className="text-white uppercase tracking-wide">Onam Offers Available rn</h1>
+        <h1 className="uppercase tracking-wide text-white">Onam Offers Available rn</h1>
       </div>
-    <div className="z-10 flex w-full items-center justify-between  bg-[#FFFFFF4D] px-16 py-5 text-black ">
-      <Image src="/logo.png" width={100} height={50} alt="" />
-      <div className="flex items-center justify-between gap-8 text-[14px]">
-        <div className="group relative inline-block hover:cursor-pointer">
-          <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
-            Home
+      <div className="z-10 flex w-full items-center justify-between  bg-[#FFFFFF4D] px-16 py-5 text-black ">
+        <Image src="/logo.png" width={100} height={50} alt="" />
+        <div className="hidden items-center justify-between gap-8 text-[14px] md:flex">
+          <div className="group relative inline-block hover:cursor-pointer">
+            <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
+              Home
+            </div>
+            <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
           </div>
-          <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
-        </div>
-        <div className="group relative inline-block hover:cursor-pointer">
-          <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
-            Collections
+          <div className="group relative inline-block hover:cursor-pointer">
+            <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
+              Collections
+            </div>
+            <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
           </div>
-          <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
-        </div>
-        <div className="group relative inline-block hover:cursor-pointer">
-          <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
-            Catalogue
+          <div className="group relative inline-block hover:cursor-pointer">
+            <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
+              Catalogue
+            </div>
+            <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
           </div>
-          <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
-        </div>
-        <div className="group relative inline-block hover:cursor-pointer">
-          <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
-            About Us
+          <div className="group relative inline-block hover:cursor-pointer">
+            <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
+              About Us
+            </div>
+            <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
           </div>
-          <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
-        </div>
-        <div className="group relative inline-block hover:cursor-pointer">
-          <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
-            Contact
+          <div className="group relative inline-block hover:cursor-pointer">
+            <div className=" font-sans uppercase tracking-wider text-[#1c1c1c] transition-all">
+              Contact
+            </div>
+            <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
           </div>
-          <span className="absolute left-0 top-6 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
         </div>
       </div>
     </div>
-      </div>
   );
 }
 
